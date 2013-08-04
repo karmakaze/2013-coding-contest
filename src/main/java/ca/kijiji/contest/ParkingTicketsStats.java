f7e5f95936ca19e51a0b389616124da95aa59ff2
Keith Kim <kkim@oanda.com>
2013-08-04 02:45:45 -0400

    Submitted version

--- src/main/java/ca/kijiji/contest/ParkingTicketsStats.java
+++ src/main/java/ca/kijiji/contest/ParkingTicketsStats.java
@@ -2,29 +2,37 @@ package ca.kijiji.contest;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
 import java.util.SortedMap;
 import java.util.TreeMap;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicIntegerArray;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
  * ParkingTicketStats
- * @author Keith Kim
+ *
+ * Principles used in this implementation:
+ * - Mechanical sympathy
+ *   - registers have fastest access, use all HyperThreads
+ *   - parallel processing
+ *   - balancing of work between workers, and between producer and consumer
+ *   - prevent cache overflows (limit read-ahead, work queue size)
+ *   - no unnecessary copying of data, or allocation of objects
+ *   - primitive collections using contiguous memory
+ * @author Keith Kim <keith.karmakaze@gmail.com>
  */
 public class ParkingTicketsStats {
 
-	// 23-bit indices (8M possible entries)
 	static final int BITS = 23;
-	static final int UNUSED_BITS = 32 - BITS;
 	static final int SIZE = 1 << BITS;
-	static final int MASK = SIZE - 1;
-	static final String[] keys = new String[SIZE];
-	static final AtomicIntegerArray vals = new AtomicIntegerArray(SIZE);
+	static final ConcurrentStringIntHashMap map = new ConcurrentStringIntHashMap(BITS);
+
 	static volatile byte[] data;
 
 	static final Pattern namePattern = Pattern.compile("([A-Z][A-Z][A-Z]+|ST [A-Z][A-Z][A-Z]+)");
@@ -38,11 +46,7 @@ public class ParkingTicketsStats {
 
     public static SortedMap<String, Integer> sortStreetsByProfitability(InputStream parkingTicketsStream) {
     	if (data != null) {
-    		Arrays.fill(keys, null);
-
-	    	for (int h = 0; h < SIZE; h++) {
-	    		vals.set(h, 0);
-	    	}
+    		map.clear();
     	}
 
     	try {
@@ -50,78 +54,23 @@ public class ParkingTicketsStats {
 
     		data = new byte[available];
 
-			Thread[] workers = new Thread[nWorkers];
-			for (int k = 0; k < nWorkers; k++) {
-				workers[k] = new Thread("worker"+ k) {
-					public void run() {
-						worker();
-					}
-				};
-				workers[k].start();
-			}
+        	Parallel.Task worker = new Parallel.Task() {
+    			public void run(int instance) {
+    				doWork();
+    			}};
 
-    		int read_end = 0;
-    		int block_start = 0;
-    		int block_end = 0;
-    		for (int read_amount = 1 * 1024 * 1024; (read_amount = parkingTicketsStream.read(data, read_end, read_amount)) > 0; ) {
-    			read_end += read_amount;
-    			block_start = block_end;
-    			block_end = read_end;
+        	List<Parallel.Task> workers = new ArrayList<Parallel.Task>();
+        	for (int i = 0; i < nWorkers; i++) {
+        		workers.add(worker);
+        	}
 
-    			// don't offer the first (header) row
-    			if (block_start == 0) {
-    				while (data[block_start++] != '\n') {}
-    			}
+        	Parallel parallel = new Parallel(workers);
 
-    			// partially read line will be processed on next iteration
-    			if (read_end < available) {
-    				while (data[--block_end] != '\n') {}
-        			block_end++;
-    			}
-
-    			// subdivide block to minimize latency and improve work balancing
-    			int sub_end = block_start;
-    			int sub_start;
-    			for (int k = 1; k <= nWorkers; k++) {
-    				sub_start = sub_end;
-    				sub_end = block_start + (block_end - block_start) * k / nWorkers;
-    				if (read_amount < available || k < nWorkers) {
-    					while (data[--sub_end] != '\n') {}
-    					sub_end++;
-    				}
-        			for (;;) {
-    					try {
-    						byteArrayQueue.put(new int[] {sub_start, sub_end});
-    						break;
-    					}
-    					catch (InterruptedException e) {
-    						e.printStackTrace();
-    					}
-        			}
-    			}
-
-    			if (available - read_end < read_amount) {
-    				read_amount = available - read_end;
-    			}
-    		}
-
-    		for (int t = 0; t < nWorkers; t++) {
-    			try {
-					byteArrayQueue.put(END_OF_WORK);
-				}
-    			catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-    		}
+        	// workers are running, send the data
+        	produceWork(parkingTicketsStream, available);
 
     		// wait for workers to finish processing last unit
-	    	for (Thread t: workers) {
-	    		try {
-					t.join();
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-	    	}
+        	parallel.waitForCompletion();
     	}
     	catch (IOException e) {
 			e.printStackTrace();
@@ -129,43 +78,99 @@ public class ParkingTicketsStats {
 
     	final SortedMap<String, Integer> sorted = new TreeMap<String, Integer>(new Comparator<String>() {
 			public int compare(String o1, String o2) {
-				int c = get(o2) - get(o1);
+				int c = map.get(o2) - map.get(o1);
 				if (c != 0) return c;
 				return o2.compareTo(o1);
 			}});
 
+    	final Map<String, Integer> syncSorted = Collections.synchronizedMap(sorted);
+
     	final int nGatherers = 2;
-    	Thread[] threads = new Thread[nGatherers];
-    	for (int t = 0; t < nGatherers; t++) {
-    		final int start = SIZE * t / nGatherers;
-    		final int end = SIZE * (t + 1) / nGatherers;
 
-        	threads[t] = new Thread(null, null, "gather"+ t, 2048) {
-        		public void run() {
-        	    	for (int i = start; i < end; i++) {
-        	    		int v = vals.get(i);
-        	    		if (v != 0) {
-        	    			synchronized (sorted) {
-        		    			sorted.put(keys[i], v);
-        	    			}
-        	    		}
-        	    	}
-        		}
-        	};
-        	threads[t].start();
-    	}
+    	Parallel.Task task = new Parallel.Task() {
+			public void run(int instance) {
+	    		int start = SIZE * instance / nGatherers;
+	    		int end = SIZE * (instance + 1) / nGatherers;
 
-    	for (Thread thread : threads) {
-	    	try { thread.join(); } catch (InterruptedException e) {}
-    	}
+    			map.putRangeTo(start, end, syncSorted);
+			}};
+
+    	Parallel.Task[] tasks = new Parallel.Task[] { task, task };
+
+    	Parallel par = new Parallel(tasks);
+    	par.waitForCompletion();
 
         return sorted;
     }
 
+    static final void produceWork(InputStream parkingTicketsStream, int available) {
+    	try {
+			int read_end = 0;
+			int block_start = 0;
+			int block_end = 0;
+			for (int read_amount = 1 * 1024 * 1024; (read_amount = parkingTicketsStream.read(data, read_end, read_amount)) > 0; ) {
+				read_end += read_amount;
+				block_start = block_end;
+				block_end = read_end;
+
+				// don't offer the first (header) row
+				if (block_start == 0) {
+					while (data[block_start++] != '\n') {}
+				}
+
+				// partially read line will be processed on next iteration
+				if (read_end < available) {
+					while (data[--block_end] != '\n') {}
+	    			block_end++;
+				}
+
+				// subdivide block to minimize latency and improve work balancing
+				int sub_end = block_start;
+				int sub_start;
+				for (int k = 1; k <= nWorkers; k++) {
+					sub_start = sub_end;
+					sub_end = block_start + (block_end - block_start) * k / nWorkers;
+
+					if (read_amount < available || k < nWorkers) {
+						while (data[--sub_end] != '\n') {}
+						sub_end++;
+					}
+
+	    			for (;;) {
+						try {
+							byteArrayQueue.put(new int[] {sub_start, sub_end});
+							break;
+						}
+						catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+	    			}
+				}
+
+				if (available - read_end < read_amount) {
+					read_amount = available - read_end;
+				}
+			}
+    	}
+    	catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		// inform each worker that there's no more work
+		for (int t = 0; t < nWorkers; t++) {
+			try {
+				byteArrayQueue.put(END_OF_WORK);
+			}
+			catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+    }
+
     /**
-     * worker parallel worker takes blocks of bytes read and processes them
+     * worker parallel worker takes blocks of bytes (with no broken lines) and processes them
      */
-    static final void worker() {
+    static final void doWork() {
 		Matcher nameMatcher = namePattern.matcher("");
 
 		// local access faster than volatile fields
@@ -217,7 +222,7 @@ public class ParkingTicketsStats {
 			    		nameMatcher.reset(location);
 			    		if (nameMatcher.find()) {
 			    			final String name = nameMatcher.group();
-			    			add(name, fine);
+			    			map.adjustOrPutValue(name, fine);
 		    			}
 					}
 				}
@@ -232,39 +237,4 @@ public class ParkingTicketsStats {
 			}
 		}
     }
-
-	public static int hash(String k) {
-		int h = 0;
-		for (char c : k.toCharArray()) {
-			int i = (c == ' ') ? 0 : ((int)c - 64) & 0x00FF;
-			h = h * 443 + i;
-			h = (h & MASK) ^ (h >>> BITS);
-		}
-		return h;
-	}
-
-	public static void add(final String k, final int d) {
-		int i = hash(k);
-
-		if (vals.getAndAdd(i, d) == 0) {
-			keys[i] = k;
-		}
-		// use code below instead of if() above to show hash collisions
-//		if (vals.getAndAdd(i, d) != 0) {
-//			synchronized (keys) {
-//				String k0 = keys[i];
-//				if (!k.equals(k0)) {
-//					println("Key hash clash: first "+ k0 +" and "+ k);
-//				}
-//			}
-//		}
-//		else {
-//			keys[i] = k;
-//		}
-	}
-
-	public static int get(final String k) {
-		int i = hash(k);
-		return vals.get(i);
-	}
 }
